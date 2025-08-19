59ffe525b53bf04b077a5f20e80da8438fb5b21f
Wonhee Lee <2wheeh@gmail.com>
2025-08-19 20:39:44 +0900

    chore: load flower assets from bundle

--- src/lib/components/FlowerBand.svelte
+++ src/lib/components/FlowerBand.svelte
@@ -1,27 +1,14 @@
 <script lang="ts">
-	import { PUBLIC_CDN_BASE } from '$env/static/public';
-
-	const flowerImages = [
-		{
-			src: `${PUBLIC_CDN_BASE}/miscs/flower-1.png`,
-		},
-		{
-			src: `${PUBLIC_CDN_BASE}/miscs/flower-2.png`,
-		},
-		{
-			src: `${PUBLIC_CDN_BASE}/miscs/flower-3.png`,
-		},
-		{
-			src: `${PUBLIC_CDN_BASE}/miscs/flower-4.png`,
-		},
-		{
-			src: `${PUBLIC_CDN_BASE}/miscs/flower-5.png`,
-		},
-	];
+	const flowerImages = Object.values(
+		import.meta.glob<string>('$lib/assets/misc/flower-*.png', {
+			eager: true,
+			as: 'url',
+		})
+	);
 </script>
 
 <div class="flex justify-between">
 	{#each flowerImages as image}
-		<img alt="A flower" src={image.src} class="inline-block h-24" />
+		<img alt="A flower" src={image} class="inline-block h-24" />
 	{/each}
 </div>
